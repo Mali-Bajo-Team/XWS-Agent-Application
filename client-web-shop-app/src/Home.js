5c464d24732f41c69dcb7993efeaffc3489d5565
vlaksi <daksrapsody@gmail.com>
2021-06-20 11:21:03 +0200

    feat: product preview design

--- client-web-shop-app/src/Home.js
+++ client-web-shop-app/src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [products] = useState([
+        { name: 'Omeksivac', price: 15, availability: 10, pathToImage: '', id: 1 },
+        { name: 'Omlet lepinja', price: 20, availability: 3, pathToImage: '', id: 2 },
+        { name: 'Sta god', price: 10, availability: 0, pathToImage: '', id: 3 }
+    ])
+
     return (
         <div className="home">
-            <h2>Homepage</h2>
+            {products.map(product => (
+                <div className="product-preview" key={product.id} >
+                    <h2>{product.name}</h2>
+                    <p>Price per product: {product.price} $</p>
+                </div>
+            ))}
         </div>
     );
 }
