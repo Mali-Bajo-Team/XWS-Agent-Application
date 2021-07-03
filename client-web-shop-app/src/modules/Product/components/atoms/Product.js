cc0bcfd4206373698043880476ba836196dadb41
vlaksi <daksrapsody@gmail.com>
2021-07-03 17:48:43 +0200

    feature[client]: show only to the user add to the cart button on product

--- client-web-shop-app/src/modules/Product/components/atoms/Product.js
+++ client-web-shop-app/src/modules/Product/components/atoms/Product.js
@@ -1,20 +1,28 @@
 import './Product.modules.css';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../../../Auth/context/UserContext';
+import { useContext } from 'react';
 
 const Product = ({ product }) => {
+	const { user } = useContext(UserContext);
+
 	return (
 		<div className="product-preview">
 			{/* We will uncomment if we want to go to the product details !! */}
 			{/* <Link to={`/products/${product.id}`}> */}
 			<div className="headerWithAddToCart">
 				<h3>{product.name}</h3>
-				<button
-					onClick={() => {
-						console.log('add to the cart simulation');
-					}}
-				>
-					+
-				</button>
+				{user.role == 'user' ? (
+					<button
+						onClick={() => {
+							console.log('add to the cart simulation');
+						}}
+					>
+						+
+					</button>
+				) : (
+					''
+				)}
 			</div>
 			<p>Price per product: {product.price} $</p>
 			{/* </Link> */}
