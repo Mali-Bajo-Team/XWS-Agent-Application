fbc5be771915603efe08ccda15b93da666698d87
vlaksi <daksrapsody@gmail.com>
2021-07-03 17:46:04 +0200

    feature[client]: button on product to add product to the cart

--- client-web-shop-app/src/modules/Product/components/atoms/Product.js
+++ client-web-shop-app/src/modules/Product/components/atoms/Product.js
@@ -4,10 +4,20 @@ import { Link } from 'react-router-dom';
 const Product = ({ product }) => {
 	return (
 		<div className="product-preview">
-			<Link to={`/products/${product.id}`}>
-				<h2>{product.name}</h2>
-				<p>Price per product: {product.price} $</p>
-			</Link>
+			{/* We will uncomment if we want to go to the product details !! */}
+			{/* <Link to={`/products/${product.id}`}> */}
+			<div className="headerWithAddToCart">
+				<h3>{product.name}</h3>
+				<button
+					onClick={() => {
+						console.log('add to the cart simulation');
+					}}
+				>
+					+
+				</button>
+			</div>
+			<p>Price per product: {product.price} $</p>
+			{/* </Link> */}
 		</div>
 	);
 };
