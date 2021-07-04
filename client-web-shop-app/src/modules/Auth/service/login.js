b731d00791f02014d72afaeef7e6f9397fb84848
Nemanja Pualic <nemanja.pualic@jsguru.io>
2021-07-04 16:16:42 +0200

    chore: cors enabled

--- client-web-shop-app/src/modules/Auth/service/login.js
+++ client-web-shop-app/src/modules/Auth/service/login.js
@@ -1,4 +1,19 @@
+const axios = require('axios');
+
 export const login = async () => {
+	axios.get('http://localhost:3000/api/users/4')
+ 		 .then(function (response) {
+    // handle success
+    	console.log(response);
+ 	 })
+ 	 .catch(function (error) {
+    	// handle error
+     console.log(error);
+  	})
+  	.then(function () {
+  	  // always executed
+ 	 });
+
 	return {
 		id: 4,
 		username: 'bob',
