32fbb223f0826dd9ae4fd1182b9800ab1648a6f7
vlaksi <daksrapsody@gmail.com>
2021-07-04 23:10:19 +0200

    feature[server]: create a product endpoint

--- server-web-shop/src/product/controller/product.controller.ts
+++ server-web-shop/src/product/controller/product.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Delete, Get, Param, UseGuards } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { Observable, of } from 'rxjs';
 import { Product } from '../models/product.interface';
 import { ProductService } from '../service/product.service';
+import { catchError, map, tap } from 'rxjs/operators';
 import { hasRoles } from 'src/auth/decorators/roles.decorator';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
@@ -9,18 +18,25 @@ import { User, UserRole } from '../../user/models/user.interface';
 
 @Controller('product')
 export class ProductController {
+  constructor(private productService: ProductService) {}
 
-    constructor(private productService: ProductService) { }
+  @Post()
+  create(@Body() post: Product): Observable<Product | Object> {
+    return this.productService.create(post).pipe(
+      map((retProduct: Product) => retProduct),
+      catchError(err => of({ error: err.message })),
+    );
+  }
 
-    @Get(':id')
-    findOne(@Param() params): Observable<Product> {
-        return this.productService.findOne(params.id);
-    }
+  @Get(':id')
+  findOne(@Param() params): Observable<Product> {
+    return this.productService.findOne(params.id);
+  }
 
-    @hasRoles(UserRole.ADMIN)
-    @UseGuards(JwtAuthGuard, RolesGuard)
-    @Delete(':id')
-    deleteOne(@Param('id') id: string): Observable<any> {
-        return this.productService.deleteOne(Number(id));
-    }
+  @hasRoles(UserRole.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Delete(':id')
+  deleteOne(@Param('id') id: string): Observable<any> {
+    return this.productService.deleteOne(Number(id));
+  }
 }
--- server-web-shop/src/product/service/product.service.ts
+++ server-web-shop/src/product/service/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { from, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { from, Observable, pipe, throwError } from 'rxjs';
+import { switchMap, map, catchError } from 'rxjs/operators';
 import { Repository } from 'typeorm';
 import { ProductEntity } from '../models/product.entity';
 import { Product } from '../models/product.interface';
@@ -9,22 +9,32 @@ import { AuthService } from 'src/auth/services/auth.service';
 
 @Injectable()
 export class ProductService {
+  constructor(
+    @InjectRepository(ProductEntity)
+    private readonly productRepository: Repository<ProductEntity>,
+    private authService: AuthService,
+  ) {}
 
-    constructor(
-        @InjectRepository(ProductEntity) private readonly productRepository: Repository<ProductEntity>,
-        private authService: AuthService
-    ) {}
+  create(product: Product): Observable<Product> {
+    return from(this.productRepository.save(product)).pipe(
+      map((tempProduct: Product) => {
+        const { ...result } = tempProduct;
+        return result;
+      }),
+      catchError(err => throwError(err)),
+    );
+  }
 
-    findOne(id: number): Observable<Product> {
-        return from(this.productRepository.findOne({id})).pipe(
-            map((product: Product) => {
-                const {name, ...result} = product;
-                return result;
-            } )
-        )
-    }
+  findOne(id: number): Observable<Product> {
+    return from(this.productRepository.findOne({ id })).pipe(
+      map((product: Product) => {
+        const { name, ...result } = product;
+        return result;
+      }),
+    );
+  }
 
-    deleteOne(id: number): Observable<any> {
-        return from(this.productRepository.delete(id));
-    }
+  deleteOne(id: number): Observable<any> {
+    return from(this.productRepository.delete(id));
+  }
 }
