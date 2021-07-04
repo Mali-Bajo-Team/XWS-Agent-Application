0a98a6f53f3d2dedc69f45719d667f9af7f361e0
vlaksi <daksrapsody@gmail.com>
2021-07-04 12:16:49 +0200

    feature[client]: edit & delete button on product

--- client-web-shop-app/src/modules/Product/components/atoms/Product.js
+++ client-web-shop-app/src/modules/Product/components/atoms/Product.js
@@ -1,6 +1,7 @@
 import './Product.modules.css';
 import { UserContext } from '../../../Auth/context/UserContext';
 import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 
 const Product = ({ product }) => {
 	const { user } = useContext(UserContext);
@@ -9,6 +10,30 @@ const Product = ({ product }) => {
 		<div className="product-preview">
 			<div className="headerWithAddToCart">
 				<h3>{product.name}</h3>
+
+				{user.role === 'admin' ? (
+					<Link
+						onClick={() => {
+							console.log('edit simulation');
+						}}
+						to={`/products/edit/${product.id}`}
+					>
+						E
+					</Link>
+				) : (
+					''
+				)}
+				{user.role === 'admin' ? (
+					<button
+						onClick={() => {
+							console.log('delete simulation');
+						}}
+					>
+						D
+					</button>
+				) : (
+					''
+				)}
 				{user.role === 'user' ? (
 					<button
 						onClick={() => {
