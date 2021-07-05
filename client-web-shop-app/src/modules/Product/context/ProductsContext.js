2797dc99065c5d81eb20c270699aa548fabd0975
Vladislav Maksimovic <vladislav.maksimovic@jsguru.io>
2021-07-05 21:20:58 +0200

    feature[client]: custom hook to get data & get all products from db & store in local storage that data

--- client-web-shop-app/src/modules/Product/context/ProductsContext.js
+++ client-web-shop-app/src/modules/Product/context/ProductsContext.js
@@ -1,12 +1,16 @@
 import { useMemo, useState, createContext, useEffect } from 'react';
+import useFetch from '../../../common/hooks/useFetch';
 
 export const ProductsContext = createContext();
 
 const ProductsContextProvider = ({ children }) => {
-	let tempProducts = localStorage.getItem('products');
-	const [products, setProducts] = useState(
-		tempProducts ? JSON.parse(tempProducts) : []
-	);
+	const {
+		error,
+		isPending,
+		data: fetchedProducts,
+	} = useFetch('http://localhost:3000/api/product/');
+
+	const [products, setProducts] = useState(fetchedProducts);
 	const providerProducts = useMemo(
 		() => ({ products, setProducts }),
 		[products, setProducts]
@@ -16,9 +20,17 @@ const ProductsContextProvider = ({ children }) => {
 		localStorage.setItem('products', JSON.stringify(products));
 	}, [products]);
 
+	useEffect(() => {
+		console.log('\n promenjen fetched products\n');
+		setProducts(fetchedProducts);
+	}, [fetchedProducts]);
+
 	return (
 		<ProductsContext.Provider value={providerProducts}>
 			{children}
+			{console.log(fetchedProducts)}
+			{console.log('error: ' + error)}
+			{console.log('is pending: ' + isPending)}
 		</ProductsContext.Provider>
 	);
 };
