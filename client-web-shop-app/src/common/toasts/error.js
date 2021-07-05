9c42a71c08d2c43bf4c60d352963bce0f116071e
Vladislav Maksimovic <vladislav.maksimovic@jsguru.io>
2021-07-05 23:26:35 +0200

    refactor[client]: changed position of the toast

--- client-web-shop-app/src/common/toasts/error.js
+++ client-web-shop-app/src/common/toasts/error.js
@@ -2,7 +2,7 @@ import { toast } from 'react-toastify';
 
 export function errorToast(message) {
 	return toast.error(message, {
-		position: 'top-right',
+		position: 'top-left',
 		autoClose: 5000,
 		hideProgressBar: false,
 		closeOnClick: true,
--- client-web-shop-app/src/common/toasts/success.js
+++ client-web-shop-app/src/common/toasts/success.js
@@ -3,7 +3,7 @@ import { toast } from 'react-toastify';
 export function successToast(message) {
 	return toast.success(message, {
 		style: { backgroundColor: '#22ad9a', color: 'white' },
-		position: 'top-right',
+		position: 'top-left',
 		autoClose: 5000,
 		hideProgressBar: false,
 		closeOnClick: true,
